66dbcd39766c762f3b3205b4ac40686d5f0146d2
Vung Nguyen <vungnguyenn1001@gmail.com>
2024-01-30 16:54:22 +0700

    change icon app

--- src/interface/IWeather.ts
+++ src/interface/IWeather.ts
@@ -1,16 +1,11 @@
+import { ICurrent } from "./ICurrent";
+import { IForecastDay } from "./IForecastDay";
+import { ILocation } from "./ILocation";
+
 export interface IWeather {
-    temp_c: number,
-    temp_f: number,
-    wind_mph: number,
-    wind_kph: number,
-    wind_degree: number,
-    pressure_mb: number,
-    pressure_in: number,
-    humidity: number,
-    cloud: number,
-    vis_km: number,
-    vis_miles: number,
-    uv: number,
-    gust_mph: number,
-    gust_kph: number,
+    location: ILocation,
+    current: ICurrent,
+    forecast: {
+        forecastday: IForecastDay[],
+    },
 }
